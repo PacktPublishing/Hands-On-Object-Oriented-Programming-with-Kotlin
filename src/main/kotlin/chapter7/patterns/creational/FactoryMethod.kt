70ca9d9eba1a702f6589d3a78683a38ab443c8f0
Ihor Kucherenko <kuchernkoigor@gmail.com>
2018-08-02 21:04:26 +0300

    Add Factory Method implementation

--- src/main/kotlin/chapter7/patterns/creational/FactoryMethod.kt
+++ src/main/kotlin/chapter7/patterns/creational/FactoryMethod.kt
@@ -1,5 +1,29 @@
 package chapter7.patterns.creational
 
 fun main(args: Array<String>) {
+    val mercedes = CarFactory().createCar(Brand.MERCEDES)
+}
 
+enum class Brand {
+    BMW,
+    MERCEDES,
+    HONDA,
+    MAZDA
+}
+
+interface Car
+class Mercedes: Car
+class BMW: Car
+class Honda: Car
+class Mazda: Car
+
+class CarFactory {
+    fun createCar(brand: Brand): Car {
+        return when(brand) {
+            Brand.BMW -> BMW()
+            Brand.MERCEDES -> Mercedes()
+            Brand.HONDA -> Honda()
+            Brand.MAZDA -> Mazda()
+        }
+    }
 }
