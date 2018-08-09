b304629c2b1786b932744567f34080d25b92f346
Ihor Kucherenko <kuchernkoigor@gmail.com>
2018-08-09 15:29:14 +0300

    Add implementation of Visitor pattern

---
+++ src/main/kotlin/chapter7/patterns/behavioral/State.kt
@@ -0,0 +1,41 @@
+package chapter7.patterns.behavioral
+
+sealed class AuthorizationState
+
+class SignedUpState: AuthorizationState()
+
+class ProfileCompletedState: AuthorizationState()
+
+class UnauthorizedState: AuthorizationState()
+
+class AuthorizationContext {
+    var state: AuthorizationState = UnauthorizedState()
+
+    fun signUp() {
+        state = SignedUpState()
+    }
+
+    fun completeProfile() {
+        state = ProfileCompletedState()
+    }
+
+    fun display() = when (state) {
+            is UnauthorizedState -> println("Display sign up screen")
+            is SignedUpState -> println("Display complete profile screen")
+            is ProfileCompletedState -> println("Display main screen")
+        }
+
+    override fun toString(): String {
+        return "AuthorizationContext(state=$state)"
+    }
+}
+
+fun main(args: Array<String>) {
+    val context = AuthorizationContext()
+    context.display()
+    context.signUp()
+    println(context)
+    context.display()
+    context.completeProfile()
+    println(context)
+}
---
+++ src/main/kotlin/chapter7/patterns/behavioral/Strategy.kt
@@ -0,0 +1,23 @@
+package chapter7.patterns.behavioral
+
+interface ImageFormatter {
+    fun format(image: Image): Image
+}
+
+class PngFormatter: ImageFormatter {
+    override fun format(image: Image) = Image()
+}
+
+class JpegFormatter: ImageFormatter {
+    override fun format(image: Image) = Image()
+}
+
+class Image {
+    fun convert(formatter: ImageFormatter): Image = formatter.format(this)
+}
+
+fun main() {
+    val image = Image()
+    val pngImage = image.convert(PngFormatter())
+    val jpegImage = image.convert(JpegFormatter())
+}
